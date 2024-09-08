33913ecdc9ce3697d5bd3a3225fe671c0bd80d40
suthardeeepak14 <suthardeepak257@gmail.com>
2024-09-08 14:55:34 +0530

    changes

--- data.js
+++ data.js
@@ -1,19 +1,19 @@
 export const initialFriends = [
   {
     id: 118836,
-    name: "Clark",
+    name: "Nitin",
     image: "https://i.pravatar.cc/48?u=118836",
     balance: -7,
   },
   {
     id: 933372,
-    name: "Sarah",
+    name: "Sneha",
     image: "https://i.pravatar.cc/48?u=933372",
     balance: 20,
   },
   {
     id: 499476,
-    name: "Anthony",
+    name: "Deepak",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
